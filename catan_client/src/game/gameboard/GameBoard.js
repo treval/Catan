33a688d103e5885c7c987ea46758e773dbcd5156
Trevor Levey <trvlev@gmail.com>
2019-08-02 16:30:59 -0400

    install babylonjs

--- catan_client/src/game/gameboard/GameBoard.js
+++ catan_client/src/game/gameboard/GameBoard.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import NotFound from '../../common/NotFound';
 import ServerError from '../../common/ServerError';
 import LoadingIndicator  from '../../common/LoadingIndicator';
-import HexGrid from './HexGrid'
 
 import { getGameById } from '../../util/APIUtils';
 
@@ -17,41 +16,6 @@ import {
 import { List } from 'antd';
 
 class GameBoard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			game: null,
-			isLoading: false
-		}
-		this.loadGameBoard = this.loadGameBoard.bind(this);
-	}
-
-	loadGameBoard(gameid) {
-		this.setState({
-			isLoading: true,
-		});
-		getGameById(gameid).then(response => {
-			this.setState({
-				isLoading: false,
-				game: response
-			});
-		}).catch(error => {
-            if(error.status === 404) {
-                this.setState({
-                    notFound: true,
-                });
-            } else {
-                this.setState({
-                    serverError: true,
-                    isLoading: false
-                });        
-            }
-        });    
-	}
-
-	componentDidMount() {
-        this.loadGameBoard(this.props.match.params.gameid);
-    }
 
 	render() {
 		return(
